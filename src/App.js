d6044ab61eaaeb282b3ab0e6b85decda9d6356d0
Claire Marchal <clairemarchal@mbp-de-claire.home>
2019-11-07 21:31:39 +0100

    exo2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React, { useState } from "react";
 import "./App.css";
-
 // 1. Faire la mise en page (html / css)
 // 2. Créer les états
 // 3. Mettre en place les interactions (onClick, etc.)
-
 const App = () => {
   const [currency1, setCurrency1] = useState("");
   const [currency2, setCurrency2] = useState("");
-  let rates = 1.139283;
+  const [currentTarget, setCurrentTarget] = useState("");
+  let rate1 = 1.139283;
+  let rate2 = 0.860717;
 
   const handleSubmit = event => {
     event.preventDefault(); // Pour empêcher le navigateur de changer de page lors de la soumission du formulaire
-    if (event.target.name === "Currency1") {
-      setCurrency2(currency1 * rates);
+    //alert(currentTarget);
+    if (currentTarget === "button1") {
+      setCurrency2(currency1 * rate1);
     } else {
-      setCurrency1(currency2 * 0.860717);
+      setCurrency1(currency2 * rate2);
     }
   };
 
@@ -28,7 +29,6 @@ const App = () => {
             <input
               placeholder="0"
               type="text"
-              name="Currency1"
               value={currency1}
               onChange={event => {
                 setCurrency1(event.target.value);
@@ -36,8 +36,24 @@ const App = () => {
             />
             <span className="logo">€</span>
           </div>
-          <button type="submit">{"-> € to $"}</button>
-          <button type="submit">{"<- $ to €"}</button>
+          <button
+            name="button1"
+            onClick={event => {
+              setCurrentTarget(event.target.name);
+            }}
+            type="submit"
+          >
+            {"-> € to $"}
+          </button>
+          <button
+            name="button2"
+            onClick={event => {
+              setCurrentTarget(event.target.name);
+            }}
+            type="submit"
+          >
+            {"<- $ to €"}
+          </button>
           <div className="input+logo">
             <input
               placeholder="0"
@@ -55,5 +71,4 @@ const App = () => {
     </div>
   );
 };
-
 export default App;
