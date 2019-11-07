3b36770b76ee7513cde86f050e84908f25168061
Claire Marchal <clairemarchal@mbp-de-claire.home>
2019-11-07 20:29:26 +0100

    exo2

--- src/Converter.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Converter = () => {
-  let rates = 1.139283;
-  return <button onClick={currency2(currency1 * rates)}>-></button>;
-};
-
-export default Converter;
